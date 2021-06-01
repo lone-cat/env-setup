25c1b731ebd2ba2e15eae79bef4c41ed2c662e58
Alex <alex@melentev.net>
2021-06-01 10:35:48 +0300

    git config

--- .root_commands.dev.sh
+++ .root_commands.dev.sh
@@ -2,3 +2,6 @@
 
 # install ifconfig
 apt install net-tools
+
+git config --system user.email "alex@melentev.net"
+git config --system user.name "Alex"
--- .root_commands.sh
+++ .root_commands.sh
@@ -3,6 +3,8 @@
 echo ""
 echo "BEGIN EXECUTION OF PRIVILEGED PART..."
 
+git config --system core.autocrlf input
+
 . ./.root_commands.$environment.sh
 . ./.set_env_vars.sh
 
